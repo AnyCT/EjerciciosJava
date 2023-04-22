0da9adedc8d5f82de9c2c851eb8fad9eb9b6fd51
AnyCT <129927525+AnyCT@users.noreply.github.com>
2023-04-22 01:01:41 -0600

    Create ejercicio_33.java

---
+++ ejercicio_33.java
@@ -0,0 +1,19 @@
+package Ejercicios_Practica;
+
+public class Ejercicio_33 {
+
+	public static void main(String[] args) {
+		// Escriba un programa que imprima los números del 1 al 100
+		
+		System.out.println("Ingresa el numero 1 e imprimir hasta el 100");
+		
+		
+		for(int b=1; b<=100;  b++)
+		
+			System.out.println("b:"+ b);
+		     
+		     System.out.println(" FELICIDADES,Haz Impreso hasta el numero 100 ");
+
+	}
+
+}
